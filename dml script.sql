fb330296696141b6e93cc43e36cc9d4161d2f99b
HanByeongYeob <42908385+HanByeongYeob@users.noreply.github.com>
2020-11-22 16:31:58 +0900

    Add files via upload
    데이터 베이스 스크립트 및 단위테스트

---
+++ dml script.sql
@@ -0,0 +1,51 @@
+INSERT INTO public.tbl_user_info
+(X_USER_ID,
+X_USER_NAME,
+X_USER_BALANCE)
+VALUES
+(123,
+'카카오1',
+9999999);
+
+INSERT INTO public.tbl_user_info
+(X_USER_ID,
+X_USER_NAME,
+X_USER_BALANCE)
+VALUES
+(456,
+'카카오2',
+9999999);
+
+INSERT INTO public.tbl_user_info
+(X_USER_ID,
+X_USER_NAME,
+X_USER_BALANCE)
+VALUES
+(789,
+'카카오3',
+9999999);
+
+INSERT INTO public.tbl_room_info
+(X_ROOM_ID,
+X_ROOM_NAME,
+X_USER_ID)
+VALUES
+('kakao', '카카오방', 123);
+
+INSERT INTO public.tbl_room_info
+(X_ROOM_ID,
+X_ROOM_NAME,
+X_USER_ID)
+VALUES
+('kakao', '카카오방', 456);
+
+INSERT INTO public.tbl_room_info
+(X_ROOM_ID,
+X_ROOM_NAME,
+X_USER_ID)
+VALUES
+('kakao', '카카오방', 789);
+
+commit;
+
+
